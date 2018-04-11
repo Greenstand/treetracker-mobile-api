fc598e98bd4e330fee82ba69f0e01d7e3f9021e2
zaven <zaven@greenstand.org>
2018-04-11 02:14:24 +0000

    Typos

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.post('/auth/register', function(req, res){
     return res.status(500, 'Server Error: No credential submitted');
   }
 
-  auth.register(function(data)){
+  auth.register(function(data){
     auth.token(req.body['client_id'], req.body['client_secret']),
       function(token){
         res.status(201).json({"token": token } );
@@ -60,7 +60,7 @@ app.post('/auth/register', function(req, res){
       
 });
 
-app.post('/auth/forgot', fucntion(req, res){
+app.post('/auth/forgot', function(req, res){
   console.log("reset");
   if (!req.body || (!req.body['client_id'] )) {
     return res.status(500, 'Server Error: No credential submitted');
@@ -130,10 +130,10 @@ app.get('/favicon.ico', function(req, res) {
     res.end();
 });
 
-app.post('/trees/create', fucntion(req, res){
+app.post('/trees/create', function(req, res){
     data.createTree( req.userId, req.body, function(data){
       res.status(201).json({
-        data[0]
+        data
       });
     })
     .catch(e => console.error(e.stack));
