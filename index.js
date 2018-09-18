552a3663f47a3f0c321b88c223e4f04a23632fe5
Zaven <zaven@greenstand.org>
2018-09-18 03:11:34 +0000

    Fix typo

--- index.js
+++ index.js
@@ -122,8 +122,8 @@ app.put('/devices/', function(req, res) {
   });
 });
 
-app.post('/planters/registration'), function(req, res) {
-  data.findOrCreateUser(body.planter_identifier, req.body.first_name, req.body.last_name, req.body.organization, function(user){
+app.post('/planters/registration', function(req, res) {
+  data.findOrCreateUser(req.body.planter_identifier, req.body.first_name, req.body.last_name, req.body.organization, function(user){
 
     data.createPlanterRegistration(user.id, req.deviceId, req.body, function(data){
       res.status(200).json({
