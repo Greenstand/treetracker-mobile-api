df9507ab366341de2dab5a0cdc3751c119608793
Zaven <matthew.fullstackgis@gmail.com>
2019-04-19 08:57:11 -0700

    Organize index.spec.js

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,74 @@
+const supertest = require('supertest');
+const chai = require('chai');
+const uuid = require('uuid');
+
+const app = require('./index.js');
+const request = supertest(app);
+
+
 let authToken;
+
+describe('Planters Endpoints', () => {
+  beforeEach(() => {
+    // Get Auth Token to test endpoints
+    request.post('/auth/token')
+      .send({ "device_android_id": "SDNE78DKJLS76F" })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        authToken = res.token;
+      });
+  });
+
+  describe('POST /planter/registration', () => {
+    it('should register a planter', (done) => {
+      request.post('/planters/registration')
+        .auth('Authorization', `bearer ${authToken}`)
+        .send({
+          planter_identifier: 'asdf-asdf-asdf',
+          first_name: 'George Washington',
+          last_name: 'Carver',
+          organization: 'NAACP'
+        })
+        .expect(200)
+        .end((err, res) => {
+          if (err) throw err;
+          expect(res.data).to.not.equal(0);
+        }, done);
+    });
+  });
+});
+
+describe('Devices Endpoints', () => {
+  beforeEach(() => {
+    // Get Auth Token to test endpoints
+    request.post('/auth/token')
+      .send({ "device_android_id": "SDNE78DKJLS76F" })
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        authToken = res.token;
+      });
+  });
+
+  describe('PUT /devices/', () => {
+    it('should add device to the list of devices', (done) => {
+      request.put('/devices/')
+      .auth('Authorization', `bearer ${authToken}`)
+      .send({ device_id: 'SDNE78DKJLS76F' })
+      .set('Accept', 'application/json')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+        expect(res.data).to.not.equal(0);
+      }, done);
+    });
+  });
+});
 
 describe('Trees Endpoints', () => {
   beforeEach(() => {
@@ -76,3 +146,4 @@ describe('Trees Endpoints', () => {
     });
   });
 });
+
