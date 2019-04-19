ebd34dcc8aff0dbe8a3aa15a9cd24c4a6854a7d1
Mitul Patel <39785177+bittricky@users.noreply.github.com>
2019-04-19 16:47:09 -0400

    tests & catch on failed promise & npm script

--- index.spec.js
+++ index.spec.js
@@ -13,9 +13,9 @@ describe('API', () => {
   before((done) => {
     // Get Auth Token to test endpoints
     request.post('/auth/token')
-      .send({ "device_android_id": "SDNE78DKJLS76F", 
+      .send({ "device_android_id": "SDNE78DKJLS76F",
         "client_id" : "9995c56e-0192-4ba2-b91e-d77fb14ec887",
-        "client_secret" : "096c03d5-6cb3-4fc7-90dc-883ecec90b84" 
+        "client_secret" : "096c03d5-6cb3-4fc7-90dc-883ecec90b84"
       })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
@@ -28,15 +28,15 @@ describe('API', () => {
   });
 
   after(() => {
-    //app.close(); // so that mocha will exit  
+    //app.close(); // so that mocha will exit
     //this only works when app = server.app.listen()
   });
 
   describe('Planters Endpoints', () => {
-   
+
     describe('POST /planter/registration', () => {
       it('should register a planter', (done) => {
-        console.log('sending token:');
+        console.log('planters/registration - sending token:');
         console.log(authToken);
         request.post('/planters/registration')
           .send({
@@ -61,14 +61,16 @@ describe('API', () => {
     describe('PUT /devices/', () => {
       it('should add device to the list of devices', (done) => {
         request.put('/devices/')
-          .auth('Authorization', `bearer ${authToken}`)
-          .send({ device_id: 'SDNE78DKJLS76F' })
-          .set('Accept', 'application/json')
+          .send({
+            deviceId: 'SDNE78DKJLS76F'
+          })
+          .set('Authorization', `Bearer ${authToken}`)
           .expect(200)
           .end((err, res) => {
             if (err) throw err;
             expect(res.data).to.not.equal(0);
-          }, done);
+            done();
+          });
       });
     });
   });
@@ -78,47 +80,51 @@ describe('API', () => {
     describe('GET /trees', () => {
       it('should return a list of trees', (done) => {
         request.get('/trees')
-          .auth('Authorization', `bearer ${authToken}`)
+          .set('Authorization', `Bearer ${authToken}`)
           .expect(200)
           .end((err, res) => {
             if (err) throw err;
             expect(res.rows).to.not.equal(0);
-          }, done);
+            done();
+          });
       });
 
       it('should not return an active property of false', (done) => {
         request.get('/trees')
-          .auth('Authorization', `bearer ${authToken}`)
+          .set('Authorization', `Bearer ${authToken}`)
           .expect(200)
           .end((err, res) => {
             if (err) throw err;
-            res.rows.forEach(x => expect(x.active).to.not.equal(false));
-          }, done)
+            res.body.forEach(x => expect(x.active).to.not.equal(false));
+            done();
+          })
       });
     });
 
     describe('GET /trees/details/user', () => {
       it('should return detailed information about the user', (done) => {
         request.get('/trees/details/user')
-          .auth('Authorization', `bearer ${authToken}`)
           .send({ userId: 'asdf-asdf-asdf' })
+          .set('Authorization', `Bearer ${authToken}`)
           .expect('Content-Type', /json/)
           .expect(200)
           .end((err, res) => {
             if (err) throw err;
             expect(res.rows).to.not.equal(0);
-          }, done);
+            done();
+          });
       });
 
       it('should not return an active property of false', (done) => {
         request.get('/trees/details/user')
-          .auth('Authorization', `bearer ${authToken}`)
           .send({ userId: 'asdf-asdf-asdf' })
+          .set('Authorization', `Bearer ${authToken}`)
           .expect('Content-Type', /json/)
           .expect(200)
           .end((err, res) => {
             if (err) throw err;
-            res.rows.forEach((x) => expect(x.active).to.not.equal(false));
+            res.body.forEach((x) => expect(x.active).to.not.equal(false));
+            done();
           });
       });
     });
@@ -126,17 +132,26 @@ describe('API', () => {
     describe('POST /trees/create', () => {
       it('should record a new tree', (done) => {
         request.post('/trees/create')
-          .auth('Authorization', `bearer ${authToken}`)
-          .send({ uuid: 'asdf-asdf-asdf' })
+          .send({
+            planter_identifier: 'asdf-asdf-asdf',
+            lat: 80,
+            lon: 120,
+            gps_accuracy: 1,
+            note: 'my note',
+            timestamp: 1536367800,
+            image_url: 'http://www.myimage.org/',
+            sequence_id: 1
+          })
+          .set('Authorization', `Bearer ${authToken}`)
           .set('Accept', 'application/json')
           .expect(201)
           .end((err, res) => {
-            if (err) throw err;
-            expect(res.data).to.not.equal(0);
-          }, done);
+            if (err) throw done(err);
+            expect(res.rows).to.not.equal(0);
+            done();
+          });
       });
     });
   });
 
 });
-
